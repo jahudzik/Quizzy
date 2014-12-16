0c5fb1624a8b826d0b59ca40d2191a089ef7f6b8
Jan Hudzik <jahudzik@gmail.com>
2014-12-16 09:11:14 +0100

    Unit tests of Question and Category entities

---
+++ app/src/androidTest/java/pl/jahu/quizzy/models/QuestionTest.java
@@ -0,0 +1,71 @@
+package pl.jahu.quizzy.models;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Quizzy
+ * Created by jahudzik on 2014-12-15.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class QuestionTest {
+
+    @Test
+    public void equals_test1() {
+        Question question = new Question("question", "answer", "category");
+        assertFalse(question.equals(null));
+    }
+
+    @Test
+    public void equals_test2() {
+        Question question = new Question("question", "answer", "category");
+        String otherObject = "aa";
+        assertFalse(question.equals(otherObject));
+    }
+
+    @Test
+    public void equals_test3() {
+        Question question = new Question("question", "answer", "category");
+        assertTrue(question.equals(question));
+    }
+
+    @Test
+    public void equals_test4() {
+        Question question1 = new Question("question", "answer", "category");
+        Question question2 = new Question("question", "answer", "category");
+        assertTrue(question1.equals(question2));
+    }
+
+    @Test
+    public void equals_test5() {
+        Question question1 = new Question("question1", "answer", "category");
+        Question question2 = new Question("question2", "answer", "category");
+        assertFalse(question1.equals(question2));
+    }
+
+    @Test
+    public void equals_test6() {
+        Question question1 = new Question("question1", null, "category");
+        Question question2 = new Question("question2", "answer", "category");
+        assertFalse(question1.equals(question2));
+    }
+
+
+    @Test
+    public void hashCode_test1() {
+        Question question1 = new Question("question", "answer", "category");
+        Question question2 = new Question("question", "answer", "category");
+        assertEquals(question1.hashCode(), question2.hashCode());
+    }
+
+    @Test
+    public void hashCode_test2() {
+        Question question1 = new Question("question", "answer", "category1");
+        Question question2 = new Question("question", "answer", "category2");
+        assertNotEquals(question1.hashCode(), question2.hashCode());
+    }
+
+}
--- app/src/main/java/pl/jahu/quizzy/models/Category.java
+++ app/src/main/java/pl/jahu/quizzy/models/Category.java
@@ -12,4 +12,23 @@ public class Category {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof Category) {
+            Category other = (Category)obj;
+            return (name.equals(other.name));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
 }
